f2593801ccda725dcfe8b8abcfa60e9367c68013
philipNgungi <philophilo037@gmail.com>
2023-01-26 22:28:28 +0300

    add encode token to user controller + encode token method in the application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::API
+    #add the encode_token method #here! since the user model is inheriting from it
+    def encode_token(payload)
+        JWT.encode(payload, 'my_s3cr3t')
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,9 @@ class UsersController < ApplicationController
     def create
         user = User.create(user_params)
         if user.valid?
-            render json: { user: UserSerializer.new(user) }, status: :created
+            #add the token which can be used to verify the user in this controller
+            token = encode_token(user_id: user.id)
+            render json: { user: UserSerializer.new(user), jwt: token }, status: :created
         else
             render json: { error: 'failed to create user' }, status: :unprocessable_entity
         end
